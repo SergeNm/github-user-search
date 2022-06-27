5bfe6a654c5027541d66a12cb137475b457199cb
Serge Nm <nmugishaserge@gmail.com>
2022-06-27 19:42:29 +0200

    test enhancement

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,5 +1,4 @@
-import {reducer as calcReducer} from "./slices/calcSlice";
-import { countriesReducer } from "./slices/countriesSlice";
+
 import { repoReducer } from "./slices/repo.slice";
 import { searchReducer } from "./slices/search.slice";
 import { themeReducer } from "./slices/theme";
@@ -7,9 +6,7 @@ import { userReducer } from "./slices/user.slice";
 import { usersReducer } from "./slices/users.slice";
 
 const reducers = {
-  calculator: calcReducer,
   theme: themeReducer,
-  countries: countriesReducer,
   users: usersReducer,
   user: userReducer,
   search: searchReducer,
